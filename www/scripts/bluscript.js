7a8340d3f903b3f38ac634af08c202a80256a162
cianneblue <cianneblue@gmail.com>
2016-06-28 00:46:56 +0900

    Project's Initial Commit

---
+++ www/scripts/bluscript.js
@@ -0,0 +1,28 @@
+function Incorrect(){
+    //grab the element we want to manipulate
+    var wrongAnswer = document.getElementById("wrong1");
+    wrongAnswer.style.color = "red";
+ 
+}
+function Incorrect2(){
+    //grab the element we want to manipulate
+    var wrongAnswer = document.getElementById("wrong2");
+    wrongAnswer.style.color = "red";
+ 
+}
+function Incorrect3(){
+    //grab the element we want to manipulate
+    var wrongAnswer = document.getElementById("wrong3");
+    wrongAnswer.style.color = "red";
+ 
+}
+function Correct1(){
+    //grab the element we want to manipulate
+    var wrongAnswer = document.getElementById("right1");
+    wrongAnswer.style.color = "blue";
+ 
+}
+function Incorrect(){
+    //grab the element we want to manipulate
+    var wrongAnswer = document.getElementById("wrong1");
+    wrongAnswer.style.color = "red";
