ef0ab5db6067345d8aa106ad561b5c4396c563b2
cianneblue <cianneblue@gmail.com>
2016-06-30 12:11:49 +0900

    added background photo and changed the front font and added a shadow animation

--- www/scripts/ques1.js
+++ www/scripts/ques1.js
@@ -49,27 +49,5 @@ function Incorrect(){
 }
 
 
-// Snow Falling
-    function fallingSnow() {
-    
-        var snowflake = $('<div class="snowflakes"></div>');
-        $('#snowZone').prepend(snowflake);
-        snowX = Math.floor(Math.random() * $('#site').width());
-        snowSpd = Math.floor(Math.random() + 5000);
 
-        snowflake.css({'left':snowX+'px'});
-        snowflake.animate({
-            top: "500px",
-            opacity : "0",
 
-        }, snowSpd, function(){
-            $(this).remove();
-            fallingSnow();
-        });
-
-    }
-    var timer = Math.floor(Math.random() +1000);
-    
-    window.setInterval(function(){
-        fallingSnow();
-    }, timer);
